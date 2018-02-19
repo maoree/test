21932d68cfc10e77b061c5a18fc0ca9ee88b05f8
maoree <begeneil@gmail.com>
2018-02-19 19:20:40 +1100

    test

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
-print ("changed")
-print ("test")
-print ("")
 
+def multsum(below):
+
+    count3  = below // 3
+    count5  = below // 5
+    low3    = 3
+    low5    = 5
+    high3   = count3 * 3
+    high5   = count5 * 5
+    sum3    = (low3 + high3) * count3 // 2
+    sum5    = (low5 + high5) * count5 // 2
+
+    return sum3 + sum5
+
+print(multsum(9))
